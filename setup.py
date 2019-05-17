92e010a17495b0534ca169c7e73ae48895d046fe
Ilya Kogan <ikogan@mythicnet.org>
2019-05-17 18:04:27 -0400

    Fix pypi license tag.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Intended Audience :: System Administrators",
         "Operating System :: OS Independent",
         "Topic :: Software Development",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
