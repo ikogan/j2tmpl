a4211d1b90cf3932b61fc73116e41f8e44a52d2c
Ilya Kogan <ikogan@mythicnet.org>
2019-05-17 16:44:42 -0400

    Fix support for Python < 3.7

--- tests/test_render.py
+++ tests/test_render.py
@@ -6,7 +6,7 @@ TEST_TEMPLATE_PATH = os.path.join(os.getcwd(), "tests", "templates")
 
 
 def test_simple_template(tmp_path, common_environment):
-    output = tmp_path / "unit.output"
+    output = os.path.join(tmp_path, "unit.output")
     args = Namespace(
         template=os.path.join(TEST_TEMPLATE_PATH, "simple.jinja"),
         output=output,
@@ -14,10 +14,12 @@ def test_simple_template(tmp_path, common_environment):
 
     cli.render(args, raw_context=common_environment)
 
-    assert output.read_text() == """_=whatisthis
+    output = open(output)
+    assert output.read() == """_=whatisthis
 LANG=en_US.UTF-8
 TERM_PROGRAM=vscode
 TERM_PROGRAM_VERSION=7
 XPC_FLAGS=0x0
 CAMEL_CASE_VARIABLE=handlethistoo
 JAVA_camelCaseVariable=thisshouldbefun"""
+    output.close()
