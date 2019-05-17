60ed8108ab2475498bc2522eae9b9fc06093698a
Ilya Kogan <ikogan@mythicnet.org>
2019-05-17 17:06:01 -0400

    Don't use the tmp_path fixture.

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,20 +1,23 @@
 import os
 from j2tmpl import cli
 from argparse import Namespace
+from tempfile import NamedTemporaryFile
 
 TEST_TEMPLATE_PATH = os.path.join(os.getcwd(), "tests", "templates")
 
 
-def test_simple_template(tmp_path, common_environment):
-    output = tmp_path/'unit.output'
+def test_simple_template(common_environment):
+    # The tmp_path fixture is not relaible across Python versions.
+    # Do this ourselves.
+    tmpfile = NamedTemporaryFile()
     args = Namespace(
         template=os.path.join(TEST_TEMPLATE_PATH, "simple.jinja"),
-        output=output,
+        output=tmpfile.name,
         encoding='utf-8')
 
     cli.render(args, raw_context=common_environment)
 
-    output = open(output)
+    output = open(tmpfile.name)
     assert output.read() == """_=whatisthis
 LANG=en_US.UTF-8
 TERM_PROGRAM=vscode
@@ -23,3 +26,4 @@ XPC_FLAGS=0x0
 CAMEL_CASE_VARIABLE=handlethistoo
 JAVA_camelCaseVariable=thisshouldbefun"""
     output.close()
+    tmpfile.close()
