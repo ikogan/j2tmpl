d3acef60f43e1ed75045aa45a2f2196a7af32c05
Ilya Kogan <ikogan@mythicnet.org>
2020-01-22 10:35:15 -0500

    Bumping version to prep for release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_requires = ["pytest", "flake8", "pytest-cover", "pytest-flake8"]
 
 setup(
     name="j2tmpl",
-    version="0.0.11",
+    version="0.0.12",
     author="Ilya Kogan",
     author_email="kogan@ohio.edu",
     url="https://github.com/ikogan/j2tmpl",
